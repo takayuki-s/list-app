491a42d88e82b70d26557efeaa018f8ede6cb56d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-12 21:57:36 +0900

    生成されたdivに「完了」「削除」ボタン追加（機能はまだ）

--- src/index.js
+++ src/index.js
@@ -7,14 +7,25 @@ const onClickAdd = () => {
 
   // div生成
   const div = document.createElement("div");
-  div.className = "list.row";
+  div.className = "list-row";
 
   // liタグ生成
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // 完了ボタン生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  console.log(completeButton);
+
+  //削除ボタン生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
