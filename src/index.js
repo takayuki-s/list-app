9c4f5d31913a812f3dd338d0b34630bd84372dcd
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-14 09:31:38 +0900

    削除ボタンクリックでイベント発火

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ const onClickAdd = () => {
   //削除ボタン生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
